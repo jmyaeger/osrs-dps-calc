0299e9ba38bcf74b8818b006701f31111e366e45
Jonathan Yaeger <jmyaeger@gmail.com>
2026-07-12 23:28:23 +0200

    Revert hardcoded shortlink URL

--- src/app/components/ShareModal.tsx
+++ src/app/components/ShareModal.tsx
@@ -10,7 +10,7 @@ const ShareModal: React.FC = observer(() => {
   const store = useStore();
   const { ui, debug } = store;
   const inputRef = createRef<HTMLInputElement>();
-  const domain = 'https://dps.oriontools.dev/';
+  const domain = process.env.NEXT_PUBLIC_SHORTLINK_URL;
   const [shareId, setShareId] = useState('');
   const [error, setError] = useState(false);
 
